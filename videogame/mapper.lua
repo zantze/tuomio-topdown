4fe14cbfb310474b3d8b12cb9009e2e13b8c1e2c
Tuomas Pöyry <munkkeligaming@gmail.com>
2015-05-02 22:26:03 +0300

    Fixed bugs

--- videogame/mapper.lua
+++ videogame/mapper.lua
@@ -17,16 +17,16 @@ function mapper.load(path)
   lines = love.filesystem.read(path)
   lines = split(lines, '\r\n')
   conf = split(lines[1], ' ')
-  map.width = conf[1]
-  map.height = conf[2]
+  map.width = tonumber(conf[1])
+  map.height = tonumber(conf[2])
   map.tilemap = {}
   map.tilemap.texture = love.graphics.newImage(conf[3])
-  map.tilemap.tile_width = conf[4]
-  map.tilemap.tile_height = conf[5]
+  map.tilemap.tile_width = tonumber(conf[4])
+  map.tilemap.tile_height = tonumber(conf[5])
   map.data = {}
-  for y = 1, map.width do
+  for y = 1, map.height do
     row = split(lines[y + 1], ' ')
-    for x = 1, map.height do
+    for x = 1, map.width do
       if map.data[x] == nil then
         map.data[x] = {}
       end
@@ -36,13 +36,13 @@ function mapper.load(path)
 
   function map:draw()
     s = 2
-    w = map.tilemap.tile_width * s
-    h = map.tilemap.tile_height * s
+    w = map.tilemap.tile_width
+    h = map.tilemap.tile_height
     t = map.tilemap.texture
     d = map.data
     for x = 1, map.width do
       for y = 1, map.height do
-        love.graphics.draw(t, love.graphics.newQuad(d[x][y] * map.tilemap.tile_width, 0, w, h, t:getWidth(), t:getHeight()), (x - 1) * w, (y - 1) * h, 0, s, s)
+        love.graphics.draw(t, love.graphics.newQuad(d[x][y] * map.tilemap.tile_width, 0, w, h, t:getWidth(), t:getHeight()), (x - 1) * (w * s), (y - 1) * (h * s), 0, s, s)
       end
     end
   end
