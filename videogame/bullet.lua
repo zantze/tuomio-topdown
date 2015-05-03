eba8f983b0f1e54fcb94abf2c8d9fc64216d7d3b
zantze <santeri.sorjonen@edu.keuda.fi>
2015-05-03 15:53:59 +0300

    fixed formating

--- videogame/bullet.lua
+++ videogame/bullet.lua
@@ -1,42 +1,24 @@
- bullet = {}
-
+bullet = {}
 
 vector = require 'vector'
 
-
 bullets = {}
 
-
-
-
-
 function bullet:load()
-    
-    sprite = love.graphics.newImage("png/bullet.png")
-    
+    sprite = love.graphics.newImage("png/bullet.png"
     end
 
 function bullet:draw()
-    
     for i, o in ipairs(bullets) do
 		love.graphics.draw(sprite, o.pos.x, o.pos.y, 0)
-	end
-    
-    
-    
+    	end
     end
 
 function bullet:update(dt)        
-    
     for i, o in ipairs(bullets) do
-    
         	o.pos = o.pos + vector(0, -speed * dt):rotated(math.rad(o.rot))
-        
         end
     end
-
-
-
 
 function bullet:spawn()
 
@@ -45,6 +27,5 @@ function bullet:spawn()
             rot = player.rot,
             speed = 200
         })
-    
     end 
 
