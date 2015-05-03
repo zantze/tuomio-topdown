3b75049695c05b211fd37b9c5e1bb3ac7f880ca4
Tuomas Pöyry <munkkeligaming@gmail.com>
2015-05-03 16:03:24 +0300

    Fixed formatting

--- videogame/bullet.lua
+++ videogame/bullet.lua
@@ -5,26 +5,25 @@ vector = require 'vector'
 bullets = {}
 
 function bullet:load()
-    end
+
+end
 
 function bullet:draw()
-    for i, o in ipairs(bullets) do
-		love.graphics.draw(sprite, o.pos.x, o.pos.y, 0)
-    	end
-    end
+  for i, o in ipairs(bullets) do
+    love.graphics.draw(sprite, o.pos.x, o.pos.y, 0)
+  end
+end
 
-function bullet:update(dt)        
-    for i, o in ipairs(bullets) do
-        	o.pos = o.pos + vector(0, -speed * dt):rotated(math.rad(o.rot))
-        end
-    end
+function bullet:update(dt)
+  for i, o in ipairs(bullets) do
+    o.pos = o.pos + vector(0, -speed * dt):rotated(math.rad(o.rot))
+  end
+end
 
 function bullet:spawn()
-
-    table.insert(bullets,{
-            pos = vector (player.pos.x,player.pos.y),
-            rot = player.rot,
-            speed = 200
-        })
-    end 
-
+  table.insert(bullets,{
+    pos = vector (player.pos.x, player.pos.y),
+    rot = player.rot,
+    speed = 200
+  })
+end
