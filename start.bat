0420b1a592252a045915f0e0538e9134a01a5136
Tuomas Pöyry <munkkeligaming@gmail.com>
2015-08-15 20:28:02 +0300

    Added music to the menu

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 echo Welcome to play the best games of your entire lives.
 echo This will be the highlight of your life
 echo Enjoy
-"love\love.exe" "./videogame"
+"..\love\love.exe" "./videogame"
--- videogame/menu.lua
+++ videogame/menu.lua
@@ -1,11 +1,24 @@
-require "scene"
+require 'scene'
 
 menu = {}
 
 function menu:load()
+    require 'modules/luafft'
+
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 	font_title = love.graphics.newFont('fonts/kenpixel.ttf', 22)
     font_text = love.graphics.newFont('fonts/kenpixel.ttf', 12)
+
+    sound = love.sound.newSoundData('music/test.mp3')
+    rate = sound:getSampleRate()
+    channels = sound:getChannels()
+    samples = sound:getSampleCount()
+
+    music = love.audio.newSource(sound)
+    music:setLooping(true)
+    music:play()
+
+    spectrum = {}
 
 	menu.girls = {}
 	menu.girls[0] = love.graphics.newImage('anime/0.jpg')
@@ -49,8 +62,11 @@ function menu:draw()
 	width = love.graphics.getWidth()
 	height = love.graphics.getHeight()
 
+    love.graphics.push()
+    local bgscale = width / 800
 	love.graphics.setColor(255, 255, 255, 128)
-	love.graphics.draw(menu.girls[menu.back], width / 2 , height / 2, 0, 1, 1, menu.girls[menu.back]:getWidth() / 2, menu.girls[menu.back]:getHeight() /2)
+	love.graphics.draw(menu.girls[menu.back], width / 2 , (height / 2) + (spectrum[2]:abs() * 100), 0, bgscale, bgscale, menu.girls[menu.back]:getWidth() / 2, menu.girls[menu.back]:getHeight() / 2)
+    love.graphics.pop()
 
 	menu.button(width / 2, (height / 2) - 50, 'Play Game', 100, 20)
 
@@ -60,12 +76,37 @@ function menu:draw()
     love.graphics.rotate(0 + math.sin(love.timer.getTime() * 2) / 32)
     love.graphics.translate(-width / 2, -height / 2)
     love.graphics.push()
+
+    color = {}
+    color.r = 0
+    color.g = 148
+    color.b = 255
+
+    local division = 10
+    local sd = (#spectrum/division);
+    for i = 1, #spectrum/division do
+        local width = width / 2
+        local dist = width / sd
+        local v = spectrum[i]
+        local n = height * 2 * v:abs(),0
+        
+        love.graphics.setColor(color.r, (color.g * (i / sd)) * (spectrum[2]:abs() * 10), color.b, (sd - (i / sd)) * 255)
+
+        love.graphics.rectangle(
+            'fill',
+            width + ((i - 1) * dist), height / 2 - n / 2,
+            10, n
+        )
+
+        love.graphics.rectangle(
+            'fill',
+            width - ((i - 1) * dist), height / 2 - n / 2,
+            10, n
+        )
+    end
+
     for i = 0, 30 do
-        color = {}
-        color.r = 0
-        color.g = 148
-        color.b = 255
-        love.graphics.setColor(color.r, (color.g * (i / 30)) * math.sin(love.timer.getTime() * 5), color.b, (i / 30) * 255)
+        love.graphics.setColor(color.r, (color.g * (i / 30)) * (spectrum[2]:abs() * 10), color.b, (i / 30) * 255)
         if(i == 30) then
         	love.graphics.setColor(255, 255, 255)
         end
@@ -90,4 +131,31 @@ function menu:update(dt)
 			menu.back = 0
 		end
 	end
+
+    -- Get the music data
+    -- TODO: Calculation breaks near the end
+    local curSample = music:tell('samples')
+    local wave = {}
+    local size = next_possible_size(2048)
+    if channels == 2 and curSample + rate < samples then
+        for i = curSample, (curSample + (size - 1) / 2) do
+            local sample = (sound:getSample(i * 2) + sound:getSample(i * 2 + 1)) * 0.5
+            table.insert(wave,complex.new(sample, 0))
+        end
+    else
+        for i = curSample, curSample + (size - 1) do
+            table.insert(wave,complex.new(sound:getSample(i), 0))
+        end
+    end
+    
+    local spec = fft(wave, false)
+    function divide(list, factor)
+        for i,v in ipairs(list) do
+            list[i] = list[i] / factor
+        end
+    end
+
+    divide(spec, size / 2)
+    spectrum = spec
 end
+
